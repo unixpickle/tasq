ab6c41108ce895461e69c868300e08c4a51ff420
Alex Nichol <unixpickle@gmail.com>
2022-05-03 11:23:12 -0400

    fix package

--- tasq-client-python/setup.py
+++ tasq-client-python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tasq-client-python",
-    py_modules=["tasq_client"],
+    packages=["tasq_client"],
     install_requires=["requests"],
-    version="0.1.0",
+    version="0.1.1",
 )
