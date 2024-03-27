eab6ce42b9f1d9fec88075d8d010806512426e0c
Alex Nichol <unixpickle@gmail.com>
2024-03-27 11:48:28 -0400

    cleaner times

--- tasq-server/homepage.go
+++ tasq-server/homepage.go
@@ -446,20 +446,33 @@ const Homepage = `<!doctype html>
 			const now = Date.now();
 			const since = Math.max(0, now - timestamp) / 1000;
 			if (since < 60) {
-				return Math.round(since) + ' second(s) ago';
-			} else if (since < 60*5) {
-				const minutes = Math.floor(since / 60);
-				const seconds = Math.round(since - minutes * 60);
-				return minutes + ' minutes and ' + seconds + ' second(s) ago';
+				const seconds = Math.round(since);
+				if (seconds == 1) {
+					return seconds + ' second ago';
+				} else {
+					return seconds + ' seconds ago';
+				}
 			} else if (since < 60*60) {
 				const minutes = Math.round(since / 60);
-				return minutes + ' minutes ago';
+				if (minutes == 1) {
+					return minutes + ' minute ago';
+				} else {
+					return minutes + ' minutes ago';
+				}
 			} else if (since < 60*60*24) {
-				const hours = (since / 60 / 60).toFixed(2);
-				return hours + ' hours ago';
+				const hours = Math.round(since / 60 / 60);
+				if (hours == 1) {
+					return hours + ' hour ago';
+				} else {
+					return hours + ' hours ago';
+				}
 			} else {
-				const days = (since / 60 / 60 / 24).toFixed(2);
-				return days + ' days ago';
+				const days = Math.round(since / 60 / 60 / 24);
+				if (days == 1) {
+					return days + ' day ago';
+				} else {
+					return days + ' days ago';
+				}
 			}
 		}
 
